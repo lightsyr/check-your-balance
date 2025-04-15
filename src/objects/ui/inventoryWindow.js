a06186859623164f262b01cc33ec1cabe5482062
Leandro Campos <contato.leandroncampos@gmail.com>
2025-04-15 15:04:20 -0300

    adição de label no inventário

--- src/objects/ui/inventoryWindow.js
+++ src/objects/ui/inventoryWindow.js
@@ -1,10 +1,39 @@
-import { GridSizer } from "phaser3-rex-plugins/templates/ui/ui-components";
+import { GridSizer, Label, Sizer } from "phaser3-rex-plugins/templates/ui/ui-components";
 import inventory from "../../data/inventory";
 import showCase from "../../data/showcase";
 import { moveItemContainer } from "../../utils/moveItemContainer";
 import { createVisualItem } from "../../utils/createVisualItem";
 
+/** 
+ * Creates an inventory window for the game.
+ * 
+ * @param {Phaser.Scene} scene - The scene to which the inventory window belongs.
+ * @param {number} x - The x position of the inventory window.
+ * @param {number} y - The y position of the inventory window.
+ * @return {GridSizer} The created inventory window.
+*/
 export default function createInventoryWindow(scene, x, y) {
+
+  const mainContainer = new Sizer(scene, {
+    x,
+    y,
+    width: 600, // Define uma largura fixa (ajuste como quiser)
+    height: 200, // Define uma altura mínima
+    orientation: "vertical",
+    anchor: "center",
+    space: {
+      item: 10
+    }
+  });
+
+  const titleLabel = new Label(scene, {
+    align: "center",
+    text: scene.add.text(0, 0, "Inventory",{
+      fontSize: "20px",
+      color: "#ffffff",
+    }),
+  })
+
   const inventoryGrid = new GridSizer(scene, {
     x, y,
     anchor: "left-top",
@@ -13,7 +42,7 @@ export default function createInventoryWindow(scene, x, y) {
     space: { column: 5, row: 5 },
   });
 
-  scene.add.existing(inventoryGrid);
+ 
 
   let itemsBoxSizer;
 
@@ -37,5 +66,9 @@ export default function createInventoryWindow(scene, x, y) {
     inventoryGrid.layout();
   };
 
+  mainContainer.add(titleLabel, { expand: false, proportion: 0 });
+  mainContainer.add(inventoryGrid, { expand: true, proportion: 1 });
+  mainContainer.layout();
+  scene.add.existing(mainContainer);
   return inventoryGrid;
 }
